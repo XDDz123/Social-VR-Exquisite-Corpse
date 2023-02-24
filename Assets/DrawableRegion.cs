d5481a441078f96e24a1bab25d6b01938dd7c736
James Maddison <james.maddison@v-nova.com>
2023-02-24 22:05:38 +0000

    add drawable region

---
+++ Assets/DrawableRegion.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawableRegion : MonoBehaviour
+{
+    [SerializeField] ComputeShader _shader;
+    RenderTexture _texture;
+
+    Vector4 _color;
+    Vector2? _lastPosition;
+
+    void Start()
+    {
+        _texture = new RenderTexture(1024, 1024, 24);
+        _texture.filterMode = FilterMode.Point;
+        _texture.enableRandomWrite = true;
+        _texture.Create();
+
+        GetComponent<Renderer>().material.mainTexture = _texture;
+
+        int clearKernel = _shader.FindKernel("Clear");
+
+        _shader.SetTexture(clearKernel, "canvas", _texture);
+        _shader.Dispatch(clearKernel, _texture.width / 8, _texture.height / 8, 1);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (!Physics.Raycast(ray, out hit, 100.0f)) {
+                return;
+            }
+
+            int updateKernel = _shader.FindKernel("Update");
+
+            Vector2 end;
+
+            end.x = hit.textureCoord.x * _texture.width;
+            end.y = hit.textureCoord.y * _texture.height;
+
+            if (_lastPosition is Vector2 start) {
+                _shader.SetVector("start", start);
+                _shader.SetVector("end", end);
+                _shader.SetVector("color", _color);
+                _shader.SetFloat("size", 4.0f);
+                _shader.SetTexture(updateKernel, "canvas", _texture);
+                _shader.Dispatch(updateKernel, _texture.width / 8, _texture.height / 8, 1);
+            }
+
+            _lastPosition = end;
+        } else {
+            _lastPosition = null;
+        }
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(_texture, dest);
+    }
+}
