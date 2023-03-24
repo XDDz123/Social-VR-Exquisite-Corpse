cc6e1f27dd436bff641a80b09caa8e23d3b6e0b5
James Maddison <james.maddison@v-nova.com>
2023-03-24 19:14:51 +0000

    doen

--- Assets/ColorSelector.cs
+++ Assets/ColorSelector.cs
@@ -5,13 +5,6 @@ using UnityEngine.UI;
 
 public class ColorSelector : MonoBehaviour
 {
-    enum Channel
-    {
-        Hue,
-        Saturation,
-        Value,
-    }
-
     public Image colorPreview;
     public Slider hueSlider;
     public Slider saturationSlider;
@@ -29,11 +22,9 @@ public class ColorSelector : MonoBehaviour
         SetupSlider(hueSlider);
         SetupSlider(saturationSlider);
         SetupSlider(valueSlider);
-        valueSlider.value = 1;
 
         _surface = GameObject.Find("Board")?.GetComponent<DrawableSurface>();
 
-        UpdateSliderBackground(hueSlider, GenerateTexture(Channel.Hue));
         UpdateColor();
     }
 
@@ -48,44 +39,9 @@ public class ColorSelector : MonoBehaviour
         slider.maxValue = 1;
     }
 
-    private Texture2D GenerateTexture(Channel channel)
-    {
-        int width = channel == Channel.Hue ? 360 : 100;
-        Color32[] colors = new Color32[width];
-
-        for (int i = 0; i < width; ++i) {
-            float h = channel == Channel.Hue ? (float)i / width : 1.0f;
-            float s = channel == Channel.Saturation ? (float)i / width : 1.0f;
-            float v = channel == Channel.Value ? (float)i / width : 1.0f;
-            Color rgb = Color.HSVToRGB(h, s, v);
-            byte r = (byte)(rgb.r * 255.0f);
-            byte g = (byte)(rgb.g * 255.0f);
-            byte b = (byte)(rgb.b * 255.0f);
-
-            colors[i] = new Color32(r, g, b, 255);
-        }
-
-        Texture2D tex = new Texture2D(1, width);
-        tex.wrapMode = TextureWrapMode.Clamp;
-        tex.SetPixels32(colors);
-        tex.Apply();
-
-        return tex;
-    }
-
-    private void UpdateSliderBackground(Slider slider, Texture texture)
-    {
-        Material material = new Material(Shader.Find("Unlit/Texture"));
-        material.mainTexture = texture;
-        slider.GetComponentInChildren<Image>().material = material;
-    }
-
     private void UpdateColor()
     {
         colorPreview.color = color;
-
-        UpdateSliderBackground(saturationSlider, GenerateTexture(Channel.Saturation));
-        UpdateSliderBackground(valueSlider, GenerateTexture(Channel.Value));
 
         if (_surface != null) {
             _surface.brushColor = color;
