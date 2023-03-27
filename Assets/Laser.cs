82361eaa17602d42d38ee39edf9b52529c944eb5
James Maddison <james.maddison@v-nova.com>
2023-03-27 16:37:43 +0100

    reconnect laser to color change

--- Assets/Laser.cs
+++ Assets/Laser.cs
@@ -5,68 +5,63 @@ using Ubiq.XR;
 
 public class Laser : MonoBehaviour
 {
-    private LineRenderer laser;
-    private Hand controller;
-    public Color color;
+    private LineRenderer _laser;
+    private Color _color = Color.black;
+
     public Vector3 PenPosition;
     public Vector3 PenDirection;
 
-    // Start is called before the first frame update
-    void Start()
+    public void UpdateColor(Color color)
     {
-        color = Color.black;
-        laser = GetComponent<LineRenderer>();
+        _color = color;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        _laser = GetComponent<LineRenderer>();
+    }
+
     void Update()
     {
-
+        GetComponent<Renderer>().enabled = false;
         RaycastHit hit;
 
-        if (Physics.Raycast(PenPosition, PenDirection, out hit, 100.0f))
+        if (!Physics.Raycast(PenPosition, PenDirection, out hit, 100.0f))
         {
-
-            MeshCollider meshCollider = hit.collider as MeshCollider;
-
-            if (meshCollider == null)
-            {
-                GetComponent<Renderer>().enabled = false;
-                return;
-            }
-
-            GetComponent<Renderer>().enabled = false;
-
-            // fixes "Texture coordinate channel "0" not found" error when accessing hit.textureCoord
-            // patch inspired by https://github.com/microsoft/MixedRealityToolkit-Unity/issues/10339
-            // and https://github.com/microsoft/MixedRealityToolkit-Unity/pull/10370
-            Mesh sharedMesh = meshCollider.sharedMesh;
-
-            if (sharedMesh == null || !sharedMesh.HasVertexAttribute(UnityEngine.Rendering.VertexAttribute.TexCoord0))
-            {
-                GetComponent<Renderer>().enabled = false;
-                return;
-            }
-
-            // checks if the hit object is the drawing canvas
-            // canvas obj's tag is set to "Canvas" in unity
-            if (!meshCollider.CompareTag("Canvas"))
-            {
-                GetComponent<Renderer>().enabled = false;
-                return;
-            }
-
-            laser.startColor = color;
-            laser.endColor = color;
-            laser.SetPosition(0, PenPosition);
-            laser.SetPosition(1, hit.point);
-
-            GetComponent<Renderer>().enabled = true;
-
+            return;
         }
-        else
+
+        MeshCollider meshCollider = hit.collider as MeshCollider;
+
+        if (meshCollider == null)
+        {
+            return;
+        }
+
+        // fixes "Texture coordinate channel "0" not found" error when accessing hit.textureCoord
+        // patch inspired by https://github.com/microsoft/MixedRealityToolkit-Unity/issues/10339
+        // and https://github.com/microsoft/MixedRealityToolkit-Unity/pull/10370
+        Mesh sharedMesh = meshCollider.sharedMesh;
+
+        if (sharedMesh == null || !sharedMesh.HasVertexAttribute(UnityEngine.Rendering.VertexAttribute.TexCoord0))
         {
             GetComponent<Renderer>().enabled = false;
+            return;
         }
+
+        // checks if the hit object is the drawing canvas
+        // canvas obj's tag is set to "Canvas" in unity
+        if (!meshCollider.CompareTag("Canvas"))
+        {
+            GetComponent<Renderer>().enabled = false;
+            return;
+        }
+
+        _laser.startColor = _color;
+        _laser.endColor = _color;
+        _laser.SetPosition(0, PenPosition);
+        _laser.SetPosition(1, hit.point);
+
+        GetComponent<Renderer>().enabled = true;
     }
 }
